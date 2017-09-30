78cf4a3fd62b4632751ba6d464f6ed869df3c5b1
stefwang1 <32406003+stefwang1@users.noreply.github.com>
2017-09-30 01:33:57 -0400

    Getters and Setters of Room Class

--- DormBase/Room.swift
+++ DormBase/Room.swift
@@ -8,19 +8,61 @@
 
 import UIKit
 
-class Room: NSObject {
+class Room: NSObject { //Variables of Room class
     var roomNo: String
     var capacity: Int
     var status: Int
     var studentEmail: String
     var password: String
     
-    init(roomNo: String, capacity: Int, status: Int, studentEmail: String, password: String){
+    init(roomNo: String, capacity: Int, status: Int, studentEmail: String, password: String){ //Constructor of Room class
         self.roomNo = roomNo
         self.capacity = capacity
         self.status = status
         self.studentEmail = studentEmail
         self.password = password
+    }
+    
+    // Setters of Room Class
+    func setRoom(newRoomNo: String) -> Void {
+        roomNo = newRoomNo
+    }
+    
+    func setCapacity(newRoomCapacity: Int) -> Void {
+        capacity = newRoomCapacity
+    }
+    
+    func setStatus(newStatus: Int) -> Void {
+        status = newStatus
+    }
+    
+    func setStudentEmail(newEmail: String) -> Void {
+        studentEmail = newEmail
+    }
+    
+    func setPassword(newPassword: String) -> Void {
+        password = newPassword
+    }
+   
+    //Getters of Room Class
+    func getRoomNo() -> String {
+        return roomNo
+    }
+    
+    func getCapacity() -> Int {
+        return capacity
+    }
+    
+    func getStatus() -> Int {
+        return status
+    }
+    
+    func getStudentEmail() -> String {
+        return studentEmail
+    }
+    
+    func getPassword() -> String {
+        return password
     }
 }
 
